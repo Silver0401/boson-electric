4c303f4f760ff6c1b19f2f9a27d0725538ccec92
Silver0401 <prosilver05@gmail.com>
2025-06-10 12:52:47 -0600

    added changes to script line tag

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -8,6 +8,7 @@ import Basket from "@/libs/Basket";
 import Nav from "@/libs/Nav";
 import { getDictionary } from "./dictionaries";
 import Script from "next/script";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Boson",
@@ -37,13 +38,17 @@ export default async function RootLayout({
   } else {
     return (
       <html lang={(await params).lang}>
-        <Script
-          src="function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-K37CKDQ3'"
-        />
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-K37CKDQ3');`,
+            }}
+          ></script>
+        </Head>
         <body>
           <GlobalContextProvider>
             <Nav lang={(await params).lang} />
